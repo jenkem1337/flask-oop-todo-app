5f77af1e790d944989d9cdf54f44349c539c8819
Can <yamahasirossi@gmail.com>
2021-09-26 16:33:44 +0300

    added login_required for updateUser

--- flask_todo_app/routes.py
+++ flask_todo_app/routes.py
@@ -24,6 +24,7 @@ def login():
     return userController.getUserForLogin()
 
 @app.route('/updateUser/<int:id>', methods=['GET','POST'])
+@login_required
 def updateUser(id):
     return userController.updateUserDetail(id)
 
